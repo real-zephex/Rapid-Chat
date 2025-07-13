3289aadb5016fc1ce4687533e7fbcd54c839cfbd
real-zephex <zephex-1@outlook.com>
2025-07-14 00:09:42 +0530

    feat: add missing import for 'off' from process; adjust ESLint rules for TypeScript

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { off } from "process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -17,6 +18,7 @@ const eslintConfig = [
       "react-hooks/exhaustive-deps": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/prefer-as-const": "off",
     },
   },
 ];
