76ee9914e1425d395214edf95baea604a7b7bb4e
real-zephex <143923795+real-zephex@users.noreply.github.com>
2025-08-11 17:49:06 +0000

    woosh

--- src/ui/chat-interface.tsx
+++ src/ui/chat-interface.tsx
@@ -76,7 +76,7 @@ const ChatInterface = ({ id }: { id: string }) => {
     return;
   }
 
-  const [model, setModel] = useState<string>("gptOssFree");
+  const [model, setModel] = useState<string>("flash_2");
   const [messages, setMessages] = useState<Message[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isUploadingImages, setIsUploadingImages] = useState(false);
@@ -145,9 +145,9 @@ const ChatInterface = ({ id }: { id: string }) => {
   //   }
   // }, [model, images]);
 
-  useEffect(() => {
-    scrollToBottom();
-  }, []);
+  // useEffect(() => {
+  //   scrollToBottom();
+  // }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
