82f73cf7a4254b452225be3b2b2ea484e8e490e2
real-zephex <zephex-1@outlook.com>
2025-12-27 23:50:15 +0530

    dependencies: changes

--- src/ui/chat-components/MessageComponent.tsx
+++ src/ui/chat-components/MessageComponent.tsx
@@ -52,7 +52,7 @@ const MessageComponent = memo(
     const displayedContent = useSmoothStream(message.content, isStreaming);
     const displayedReasoning = useSmoothStream(
       message.reasoning || "",
-      isStreaming,
+      isStreaming
     );
 
     const reasoningTokens = useMemo(() => {
@@ -67,7 +67,7 @@ const MessageComponent = memo(
         message.content
           .split(/[ \t\n\r\f.,!?;:"'’“”(){}\[\]-]+/)
           .filter(Boolean).length + reasoningTokens,
-      [message.content],
+      [message.content]
     );
 
     const tokensPerSecond = useMemo(() => {
@@ -212,7 +212,7 @@ const MessageComponent = memo(
                     const getLanguage = (element: any): string => {
                       if (element?.props?.className) {
                         const match = /language-(\w+)/.exec(
-                          element.props.className,
+                          element.props.className
                         );
                         return match ? match[1] : "";
                       }
@@ -525,7 +525,7 @@ const MessageComponent = memo(
         </div>
       </div>
     );
-  },
+  }
 );
 MessageComponent.displayName = "MessageComponent";
 
